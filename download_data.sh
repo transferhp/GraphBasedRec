513a37e286d0d74a44d958da6ba7ddf24775f33c
Peng Hao <peng.hao@student.uts.edu.au>
2016-11-13 13:06:28 +1100

    init commit

---
+++ download_data.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+DATA_DIR=./movielens
+SIZE=100k
+mkdir -p ${DATA_DIR}
+wget http://files.grouplens.org/datasets/movielens/ml-${SIZE}.zip -O ${DATA_DIR}/ml-${SIZE}.zip
+unzip ${DATA_DIR}/ml-${SIZE}.zip -d ${DATA_DIR}
